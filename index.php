fb097e532df5829b425ba4bad6c5197bd3ac5f58
Masha Kuzmicheva <kum@d-element.ru>
2023-01-15 23:35:23 +0500

    add main.feedback

--- index.php
+++ index.php
@@ -1,14 +1,21 @@
 <?
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("Hunger");
-?><?$APPLICATION->IncludeComponent(
-	"bitrix:main.feedback",
-	"",
-	Array(
-		"EMAIL_TO" => "miskusitel@gmail.com",
-		"EVENT_MESSAGE_ID" => array("7"),
-		"OK_TEXT" => "Спасибо, ваше сообщение принято.",
-		"REQUIRED_FIELDS" => array("NAME","EMAIL"),
-		"USE_CAPTCHA" => "Y"
-	)
+?><?$APPLICATION->IncludeComponent("hungry:main.feedback", "feedbackForm", Array(
+	"EMAIL_TO" => "miskusitel@gmail.com",	// E-mail, на который будет отправлено письмо
+		"EVENT_MESSAGE_ID" => array(	// Почтовые шаблоны для отправки письма
+			0 => "7",
+		),
+		"OK_TEXT" => "Спасибо, ваше сообщение принято.",	// Сообщение, выводимое пользователю после отправки
+		"REQUIRED_FIELDS" => array(	// Обязательные поля для заполнения
+			0 => "NAME",
+			1 => "EMAIL",
+		),
+		"USE_CAPTCHA" => "Y",	// Использовать защиту от автоматических сообщений (CAPTCHA) для неавторизованных пользователей
+	),
+	false
+);?><?$APPLICATION->IncludeComponent("bitrix:breadcrumb", "template1", Array(
+	
+	),
+	false
 );?><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
