185bdbfae398ce91f3f120d74c42cc134ac95ae2
Masha Kuzmicheva <kum@d-element.ru>
2023-01-15 23:35:51 +0500

    add send event

--- bitrix/php_interface/init.php
+++ bitrix/php_interface/init.php
@@ -1,1 +1,12 @@
-<?php
+<?
+AddEventHandler("mail", "OnBeforeEventSend", "ModifySaleMails");
+
+function ModifySaleMails(&$arFields, &$arTemplate)
+{
+    //получим сообщение
+    $mess = $arTemplate["PHONE"];
+    foreach($arFields as $keyField => $arField) {
+        $mess = str_replace('#' . $keyField . '#', $arField, $mess); //подставляем значения в шаблон
+    }
+}
+?>
