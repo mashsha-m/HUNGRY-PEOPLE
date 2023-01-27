47a661ced492c9058b363303c32188cb673c4f8e
Masha Kuzmicheva <kum@d-element.ru>
2023-01-27 13:38:57 +0500

    update index.php

--- index.php
+++ index.php
@@ -2,65 +2,208 @@
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("Hunger");
 ?>
-<?$APPLICATION->IncludeComponent("bitrix:news.list", "reviews", Array(
-	"ACTIVE_DATE_FORMAT" => "j M Y",	// Формат показа даты
-		"ADD_SECTIONS_CHAIN" => "N",	// Включать раздел в цепочку навигации
-		"AJAX_MODE" => "N",	// Включить режим AJAX
-		"AJAX_OPTION_ADDITIONAL" => "",	// Дополнительный идентификатор
-		"AJAX_OPTION_HISTORY" => "N",	// Включить эмуляцию навигации браузера
-		"AJAX_OPTION_JUMP" => "N",	// Включить прокрутку к началу компонента
-		"AJAX_OPTION_STYLE" => "N",	// Включить подгрузку стилей
-		"CACHE_FILTER" => "N",	// Кешировать при установленном фильтре
-		"CACHE_GROUPS" => "Y",	// Учитывать права доступа
-		"CACHE_TIME" => "36000000",	// Время кеширования (сек.)
-		"CACHE_TYPE" => "A",	// Тип кеширования
-		"CHECK_DATES" => "Y",	// Показывать только активные на данный момент элементы
-		"DETAIL_URL" => "",	// URL страницы детального просмотра (по умолчанию - из настроек инфоблока)
-		"DISPLAY_BOTTOM_PAGER" => "Y",	// Выводить под списком
-		"DISPLAY_DATE" => "Y",	// Выводить дату элемента
-		"DISPLAY_NAME" => "Y",	// Выводить название элемента
-		"DISPLAY_PICTURE" => "Y",	// Выводить изображение для анонса
-		"DISPLAY_PREVIEW_TEXT" => "Y",	// Выводить текст анонса
-		"DISPLAY_TOP_PAGER" => "N",	// Выводить над списком
-		"FIELD_CODE" => array(	// Поля
-			0 => "",
-			1 => "",
-		),
-		"FILTER_NAME" => "",	// Фильтр
-		"HIDE_LINK_WHEN_NO_DETAIL" => "N",	// Скрывать ссылку, если нет детального описания
-		"IBLOCK_ID" => "5",	// Код информационного блока
-		"IBLOCK_TYPE" => "news",	// Тип информационного блока (используется только для проверки)
-		"INCLUDE_IBLOCK_INTO_CHAIN" => "N",	// Включать инфоблок в цепочку навигации
-		"INCLUDE_SUBSECTIONS" => "N",	// Показывать элементы подразделов раздела
-		"MESSAGE_404" => "",	// Сообщение для показа (по умолчанию из компонента)
-		"NEWS_COUNT" => "20",	// Количество новостей на странице
-		"PAGER_BASE_LINK_ENABLE" => "N",	// Включить обработку ссылок
-		"PAGER_DESC_NUMBERING" => "N",	// Использовать обратную навигацию
-		"PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",	// Время кеширования страниц для обратной навигации
-		"PAGER_SHOW_ALL" => "N",	// Показывать ссылку "Все"
-		"PAGER_SHOW_ALWAYS" => "N",	// Выводить всегда
-		"PAGER_TEMPLATE" => ".default",	// Шаблон постраничной навигации
-		"PAGER_TITLE" => "Новости",	// Название категорий
-		"PARENT_SECTION" => "",	// ID раздела
-		"PARENT_SECTION_CODE" => "",	// Код раздела
-		"PREVIEW_TRUNCATE_LEN" => "",	// Максимальная длина анонса для вывода (только для типа текст)
-		"PROPERTY_CODE" => array(	// Свойства
-			0 => "RATING",
-			1 => "",
-		),
-		"SET_BROWSER_TITLE" => "N",	// Устанавливать заголовок окна браузера
-		"SET_LAST_MODIFIED" => "N",	// Устанавливать в заголовках ответа время модификации страницы
-		"SET_META_DESCRIPTION" => "N",	// Устанавливать описание страницы
-		"SET_META_KEYWORDS" => "N",	// Устанавливать ключевые слова страницы
-		"SET_STATUS_404" => "N",	// Устанавливать статус 404
-		"SET_TITLE" => "N",	// Устанавливать заголовок страницы
-		"SHOW_404" => "N",	// Показ специальной страницы
-		"SORT_BY1" => "ACTIVE_FROM",	// Поле для первой сортировки новостей
-		"SORT_BY2" => "SORT",	// Поле для второй сортировки новостей
-		"SORT_ORDER1" => "DESC",	// Направление для первой сортировки новостей
-		"SORT_ORDER2" => "ASC",	// Направление для второй сортировки новостей
-		"STRICT_SECTION_CHECK" => "N",	// Строгая проверка раздела для показа списка
-	),
-	false
+<div id="four_block" class="column">
+	<div class="column w_fb">
+		<div class="logo">
+			 DELICIOUS MENU
+		</div>
+		<div class="yellow_block">
+		</div>
+ <br>
+		<div class="text t_one">
+			 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis at velit maximus, molestie est a, tempor magna.
+		</div>
+	</div>
+	 <?$APPLICATION->IncludeComponent(
+	"bitrix:catalog.section.list",
+	"store_v3_menu1",
+	Array(
+		"ADD_SECTIONS_CHAIN" => "N",
+		"CACHE_FILTER" => "N",
+		"CACHE_GROUPS" => "Y",
+		"CACHE_TIME" => "36000000",
+		"CACHE_TYPE" => "A",
+		"COMPONENT_TEMPLATE" => "store_v3_menu",
+		"COUNT_ELEMENTS" => "Y",
+		"COUNT_ELEMENTS_FILTER" => "CNT_ACTIVE",
+		"FILTER_NAME" => "sectionsFilter",
+		"IBLOCK_ID" => "6",
+		"IBLOCK_TYPE" => "products",
+		"OFFSET_MODE" => "N",
+		"SECTION_CODE" => "",
+		"SECTION_FIELDS" => array(0=>"",1=>"",),
+		"SECTION_ID" => "",
+		"SECTION_URL" => "",
+		"SECTION_USER_FIELDS" => array(0=>"UF_NAME_EN",1=>"",),
+		"SHOW_ANGLE" => "Y",
+		"SHOW_PARENT_NAME" => "Y",
+		"TOP_DEPTH" => "2",
+		"VIEW_MODE" => "LINE"
+	)
 );?>
-<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
+    <?$APPLICATION->IncludeComponent(
+        "bitrix:catalog.section",
+        "list1",
+        array(
+            "ACTION_VARIABLE" => "action",
+            "ADD_PICT_PROP" => "-",
+            "ADD_PROPERTIES_TO_BASKET" => "Y",
+            "ADD_SECTIONS_CHAIN" => "N",
+            "AJAX_MODE" => "Y",
+            "AJAX_OPTION_ADDITIONAL" => "",
+            "AJAX_OPTION_HISTORY" => "N",
+            "AJAX_OPTION_JUMP" => "N",
+            "AJAX_OPTION_STYLE" => "N",
+            "BACKGROUND_IMAGE" => "-",
+            "BASKET_URL" => "/personal/basket.php",
+            "BROWSER_TITLE" => "-",
+            "CACHE_FILTER" => "N",
+            "CACHE_GROUPS" => "Y",
+            "CACHE_TIME" => "36000000",
+            "CACHE_TYPE" => "A",
+            "COMPATIBLE_MODE" => "N",
+            "DETAIL_URL" => "",
+            "DISABLE_INIT_JS_IN_COMPONENT" => "N",
+            "DISPLAY_BOTTOM_PAGER" => "Y",
+            "DISPLAY_COMPARE" => "N",
+            "DISPLAY_TOP_PAGER" => "N",
+            "ELEMENT_SORT_FIELD" => "sort",
+            "ELEMENT_SORT_FIELD2" => "id",
+            "ELEMENT_SORT_ORDER" => "asc",
+            "ELEMENT_SORT_ORDER2" => "desc",
+            "ENLARGE_PRODUCT" => "STRICT",
+            "FILTER_NAME" => "arrFilter",
+            "IBLOCK_ID" => "6",
+            "IBLOCK_TYPE" => "products",
+            "INCLUDE_SUBSECTIONS" => "Y",
+            "LABEL_PROP" => "",
+            "LAZY_LOAD" => "N",
+            "LINE_ELEMENT_COUNT" => "3",
+            "LOAD_ON_SCROLL" => "N",
+            "MESSAGE_404" => "",
+            "MESS_BTN_ADD_TO_BASKET" => "В корзину",
+            "MESS_BTN_BUY" => "Купить",
+            "MESS_BTN_DETAIL" => "Подробнее",
+            "MESS_BTN_LAZY_LOAD" => "Показать ещё",
+            "MESS_BTN_SUBSCRIBE" => "Подписаться",
+            "MESS_NOT_AVAILABLE" => "Нет в наличии",
+            "META_DESCRIPTION" => "-",
+            "META_KEYWORDS" => "-",
+            "OFFERS_LIMIT" => "5",
+            "PAGER_BASE_LINK_ENABLE" => "N",
+            "PAGER_DESC_NUMBERING" => "N",
+            "PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
+            "PAGER_SHOW_ALL" => "N",
+            "PAGER_SHOW_ALWAYS" => "N",
+            "PAGER_TEMPLATE" => "load-more",
+            "PAGER_TITLE" => "Товары",
+            "PAGE_ELEMENT_COUNT" => "18",
+            "PARTIAL_PRODUCT_PROPERTIES" => "N",
+            "PRICE_CODE" => array(
+                0 => "PRICE",
+            ),
+            "PROPERTY_CODE" => array(
+                0 => "DESC_EN",
+                1 => "NAME_EN",
+                2 => "DESC",
+                3 => "",
+            ),
+            "PRICE_VAT_INCLUDE" => "Y",
+            "PRODUCT_BLOCKS_ORDER" => "price,props,sku,quantityLimit,quantity,buttons",
+            "PRODUCT_ID_VARIABLE" => "id",
+            "PRODUCT_PROPS_VARIABLE" => "prop",
+            "PRODUCT_QUANTITY_VARIABLE" => "quantity",
+            "PRODUCT_ROW_VARIANTS" => "[{'VARIANT':'2','BIG_DATA':false},{'VARIANT':'2','BIG_DATA':false},{'VARIANT':'2','BIG_DATA':false},{'VARIANT':'2','BIG_DATA':false},{'VARIANT':'2','BIG_DATA':false},{'VARIANT':'2','BIG_DATA':false}]",
+            "RCM_PROD_ID" => $_REQUEST["PRODUCT_ID"],
+            "RCM_TYPE" => "personal",
+            "SECTION_CODE" => "",
+            "SECTION_ID" => "",
+            "SECTION_ID_VARIABLE" => "SECTION_ID",
+            "SECTION_URL" => "",
+            "SECTION_USER_FIELDS" => array(
+                0 => "UF_NAME_EN",
+                1 => "",
+            ),
+            "SEF_MODE" => "N",
+            "SET_BROWSER_TITLE" => "N",
+            "SET_LAST_MODIFIED" => "N",
+            "SET_META_DESCRIPTION" => "Y",
+            "SET_META_KEYWORDS" => "N",
+            "SET_STATUS_404" => "N",
+            "SET_TITLE" => "N",
+            "SHOW_404" => "N",
+            "SHOW_ALL_WO_SECTION" => "N",
+            "SHOW_FROM_SECTION" => "N",
+            "SHOW_PRICE_COUNT" => "1",
+            "SHOW_SLIDER" => "Y",
+            "SLIDER_INTERVAL" => "3000",
+            "SLIDER_PROGRESS" => "N",
+            "TEMPLATE_THEME" => "blue",
+            "USE_ENHANCED_ECOMMERCE" => "N",
+            "USE_MAIN_ELEMENT_SECTION" => "N",
+            "USE_PRICE_COUNT" => "N",
+            "USE_PRODUCT_QUANTITY" => "N",
+            "COMPONENT_TEMPLATE" => "list1",
+            "PRODUCT_PROPERTIES" => array(
+            )
+        ),
+        false
+    );?>
+</div>
+<?$APPLICATION->IncludeComponent(
+	"bitrix:news.list",
+	"reviews",
+	Array(
+		"ACTIVE_DATE_FORMAT" => "j M Y",
+		"ADD_SECTIONS_CHAIN" => "N",
+		"AJAX_MODE" => "N",
+		"AJAX_OPTION_ADDITIONAL" => "",
+		"AJAX_OPTION_HISTORY" => "N",
+		"AJAX_OPTION_JUMP" => "N",
+		"AJAX_OPTION_STYLE" => "N",
+		"CACHE_FILTER" => "N",
+		"CACHE_GROUPS" => "Y",
+		"CACHE_TIME" => "36000000",
+		"CACHE_TYPE" => "A",
+		"CHECK_DATES" => "Y",
+		"DETAIL_URL" => "",
+		"DISPLAY_BOTTOM_PAGER" => "Y",
+		"DISPLAY_DATE" => "Y",
+		"DISPLAY_NAME" => "Y",
+		"DISPLAY_PICTURE" => "Y",
+		"DISPLAY_PREVIEW_TEXT" => "Y",
+		"DISPLAY_TOP_PAGER" => "N",
+		"FIELD_CODE" => array(0=>"",1=>"",),
+		"FILTER_NAME" => "",
+		"HIDE_LINK_WHEN_NO_DETAIL" => "N",
+		"IBLOCK_ID" => "5",
+		"IBLOCK_TYPE" => "news",
+		"INCLUDE_IBLOCK_INTO_CHAIN" => "N",
+		"INCLUDE_SUBSECTIONS" => "N",
+		"MESSAGE_404" => "",
+		"NEWS_COUNT" => "20",
+		"PAGER_BASE_LINK_ENABLE" => "N",
+		"PAGER_DESC_NUMBERING" => "N",
+		"PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
+		"PAGER_SHOW_ALL" => "N",
+		"PAGER_SHOW_ALWAYS" => "N",
+		"PAGER_TEMPLATE" => ".default",
+		"PAGER_TITLE" => "Новости",
+		"PARENT_SECTION" => "",
+		"PARENT_SECTION_CODE" => "",
+		"PREVIEW_TRUNCATE_LEN" => "",
+		"PROPERTY_CODE" => array(0=>"RATING",1=>"",),
+		"SET_BROWSER_TITLE" => "N",
+		"SET_LAST_MODIFIED" => "N",
+		"SET_META_DESCRIPTION" => "N",
+		"SET_META_KEYWORDS" => "N",
+		"SET_STATUS_404" => "N",
+		"SET_TITLE" => "N",
+		"SHOW_404" => "N",
+		"SORT_BY1" => "ACTIVE_FROM",
+		"SORT_BY2" => "SORT",
+		"SORT_ORDER1" => "DESC",
+		"SORT_ORDER2" => "ASC",
+		"STRICT_SECTION_CHECK" => "N"
+	)
+);?><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
